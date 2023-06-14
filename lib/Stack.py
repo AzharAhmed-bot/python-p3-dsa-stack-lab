04d2a8a1ff35f47774f0c04b0380e2d154694aa1
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-06-14 08:07:50 +0300

    Complete assignment

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,13 +1,16 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items=items
+        self.limit=limit
+
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+        
 
     def push(self, item):
-        pass
+        self.items.append(item)
 
     def pop(self):
         pass
@@ -16,10 +19,24 @@ class Stack:
         pass
     
     def size(self):
-        pass
+        length=len(self.items)
+        return length
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
+
 
     def search(self, target):
-        pass
+        for i in range(len(self.items) - 1, -1, -1):
+            if self.items[i] == target:
+                return len(self.items) - i - 1
+        return -1
+
+    
+# stack = Stack()
+# stack.push(10)
+# stack.push(20)
+# stack.push(30)
+# stack.push(40)
+# stack.push(50)
+# print(stack.full())
